dfc79ed9d9a05c60a46bb757998bdc16f060da5d
RArielAlonso <rariel.alonso@gmail.com>
2023-09-08 14:34:35 -0300

    ejercicios clase 2

---
+++ clase2/src_ej1/modulo.py
@@ -0,0 +1,15 @@
+import sys
+import pandas as pd
+import numpy as np
+import datetime
+import os
+
+python_version=sys.version
+pandas_version=pd.__version__
+numpy_version=np.__version__
+
+#print(f'La version de python es: {python_version}')
+#print(f'La version de pandas es: {pandas_version}')
+#print(f'La version de numpy es: {numpy_version}')
+
+print(sys.modules.keys())
