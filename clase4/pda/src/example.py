ceb8d74bbaa14f622f3db1b1519baba6021c1897
RArielAlonso <rariel.alonso@gmail.com>
2023-09-19 14:42:22 -0300

    agregados clase 3 4 y 5

---
+++ clase4/pda/src/example.py
@@ -0,0 +1,13 @@
+import math
+
+def my_func(x):
+    x_proc=x+1
+    assert x_proc>=0, "El valor de x es negativo"
+    res=math.sqrt(x_proc)
+    return res
+
+if __name__=='__main__':
+    #breakpoint()
+    var=2
+    return_value=my_func(var)
+    print(return_value)
