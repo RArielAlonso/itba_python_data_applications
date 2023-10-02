4dc3547e58642f49a99b6e5f1a3b7ff7f32ee88e
RArielAlonso <rariel.alonso@gmail.com>
2023-10-02 20:24:05 -0300

    agregado clase 6: playing with parquet.py

--- clase6/playing_with_parquet.py
+++ clase6/playing_with_parquet.py
@@ -1,46 +1,56 @@
-import csv
-from faker import Faker
 import pandas as pd
 import time
-
-fake = Faker()
-
-
-def generate_random_csv(row_list):
-    for i in row_list:
-        with open(f'random-person-{i}-rows.csv', mode='w') as file:
-            file_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            file_writer.writerow(['first_name', 'last_name', 'age'])
-            for _ in range(i):
-                file_writer.writerow([fake.first_name(), fake.last_name(), fake.numerify("@#")])
+import numpy as np
+import os
 
 
-def to_parquet_to_compression(row_list, compression):
-    for i in row_list:
-        print(i)
-        start_reading_time = time.time()
-        data_csv = pd.read_csv(f'random-person-{i}-rows.csv')
-        finished_reading_time = time.time()
-        delta_reading_time = finished_reading_time-start_reading_time
-        print("--------------------------")
-        print(f"For {i} numbers of rows, reading time was {delta_reading_time}")
-        print("--------------------------")
-        for j in compression:
-            start_compression_time = time.time()
-            print(start_compression_time)
-            data_csv.to_parquet(f'random-person-{i}-rows-{j}-compression.parquet', compression=j)
-            end_compression_time = time.time()
-            delta_compression_time = end_compression_time-start_compression_time
-            print("--------------------------")
-            print(f"For {i} numbers of rows and compression {j}, compression time was {delta_compression_time}")
-            print("--------------------------")
+def generate_random_csv(size):
+    df = pd.DataFrame()
+    df['name'] = np.random.choice(['Ariel', 'Miguel', 'Nicolas','Florencia', 'Ana'], size)
+    df['last_name'] = np.random.choice(['Alonso', 'Sanchez', 'Fernandez', 'Martinez', 'Peruzzi'], size)
+    df['age'] = np.random.randint(1, 100, size)
+    df['liter_water_per_day'] = np.random.uniform(0, 4, size)
+    df['needs_more_water'] = np.random.choice([True, False], size)
+    df.to_csv(f"random-person-{size}-rows.csv")
+
+
+def read_csv(size):
+    data_csv = pd.read_csv(f'random-person-{size}-rows.csv')
+    return data_csv
+
+
+def to_parquet(df,size,compression):
+     df.to_parquet(f'random-person-{size}-rows-{compression}-compression.parquet', compression=compression)
 
 
 def main():
-    number_of_records = [10, 1000, 10000, 100000, 1000000, 5000000]
-    # generate_random_csv(number_of_records)
+    number_of_records = [10000, 100000, 1000000, 5000000, 10_000_000]
     compression = ["snappy", "gzip", "brotli", "lz4", "zstd"]
-    to_parquet_to_compression(number_of_records, compression)
+    results_dict_csv = {}
+    for i in number_of_records:
+        start_generating_csv_time = time.time()
+        generate_random_csv(i)
+        end_generating_csv_time = time.time()
+        start_reading_time = time.time()
+        data_csv = read_csv(i)
+        finished_reading_time = time.time()
+        for j in compression:
+            start_compression_time = time.time()
+            to_parquet(data_csv, i, compression=j)
+            end_compression_time = time.time()
+            delta_generation_csv = end_generating_csv_time-start_generating_csv_time
+            delta_reading_time = finished_reading_time-start_reading_time
+            delta_compression_time = end_compression_time-start_compression_time
+            results_dict_csv[i,j] = {
+                "rows": int(i),
+                "compression": j,
+                "time_generation_csv": round(delta_generation_csv,2),
+                "time_reading_csv": round(delta_reading_time,2),
+                "compression_time": round(delta_compression_time,2),
+                "parquet_size_kb": round(os.path.getsize(f'random-person-{i}-rows-{j}-compression.parquet')/1024**1),
+                "csv_size_kb": round(os.path.getsize(f'random-person-{i}-rows.csv')/1024**1)
+            }        
+    pd.DataFrame(results_dict_csv).to_excel("Results.xlsx")
 
 
 if __name__ == "__main__":
